d177c56a8d2931ed8ecfcc343ea5c5e94240bb95
Paula Rösler <paularoesler@web.de>
2021-02-23 16:14:45 +0100

    Small corrections to GlobalStyle component

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,6 +1,6 @@
-import { createGlobalStyles } from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 
-export default createGlobalStyles`
+export default createGlobalStyle`
 * {
     box-sizing: border-box;
   }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import './Button.css'
+import styled from 'styled-components/macro'
 
-export default function Button({ text, onClick }) {
-  return (
-    <button className="Button" onClick={onClick}>
-      {text}
-    </button>
-  )
-}
+export default styled.button`
+  .Button {
+    padding: 5px;
+    background: #bbb;
+    width: 100%;
+  }
+
+  .Button--active {
+    background: #333;
+    color: white;
+  }
+`
