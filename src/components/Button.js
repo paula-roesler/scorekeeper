7f547c10d70772370548c7dfa24918ee2677af7e
Paula Rösler <paularoesler@web.de>
2021-02-24 16:12:23 +0100

    Started including function

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,6 +4,7 @@ export default styled.button`
   width: 100%;
   height: 40px;
   padding: 5px;
-  background-color: ${props => (props.bgColor ? props.bgColor : 'hotpink')};
-  border: 1px solid #888;
+  background-color: ${props => (props.bgColor ? props.bgColor : 'royalblue')};
+  color: white;
+  border: none;
 `
